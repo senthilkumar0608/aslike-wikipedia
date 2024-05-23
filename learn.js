14b730606f5ad9248c0cbf3f5062ccac4513c702
senthilkumar0608 <senthilkumar06082003@gmail.com>
2024-05-23 22:27:32 +0530

    script

--- learn.js
+++ learn.js
@@ -1,2 +1,11 @@
 // There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+document.addEventListener("DOMContentLoaded", function() {
+    const photo = document.getElementById("clickable-photo");
+    
+    photo.addEventListener("click", function() {
+        alert("Photo clicked!");
+        // You can perform any other actions here
+        console.log("The photo was clicked.");
+    });
+});
 
